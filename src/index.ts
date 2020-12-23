1386b1e182d5c81a132fcc04a73a7fc95de289d1
Pal Kerecsenyi <palkerecs@gmail.com>
2020-12-23 20:00:07 +0000

    Add CORS support in Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import get from './routes/get';
 import save from './routes/save';
 import clone from './routes/clone';
@@ -7,6 +8,11 @@ import clone from './routes/clone';
 const app = express();
 app.set('trust proxy', true);
 app.use(bodyParser.json());
+
+app.use(cors({
+    origin: ['https://palcode.dev', 'http://localhost:3000'],
+    credentials: true,
+}));
 
 app.use(get);
 app.use(save);
